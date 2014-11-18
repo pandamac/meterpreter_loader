3fe559409b4bc8b91a72e1c3e850ccb397f0c5ff
panda <694918610@qq.com>
2014-11-18 16:45:13 +0800

    testcommit

--- MpCsLoader/Program.cs
+++ MpCsLoader/Program.cs
@@ -43,7 +43,7 @@ namespace MpCsLoader
                     int sockfd = StageLoader.LoadBinData("107.170.234.111", 1433);
                     func(sockfd);
                 }
-            }
+            }//hehe
             catch(Exception x)
             {
                 Console.WriteLine(x.ToString());
